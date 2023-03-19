58d0931d4d892b24b6ece8940452c8c38e1b31d8
pva <tango2002@gmail.com>
2023-03-19 21:22:17 +0300

    fix: add exception to next()

--- src/by/tms/lesson18/homework/listrealization/ArrayListIterator.java
+++ src/by/tms/lesson18/homework/listrealization/ArrayListIterator.java
@@ -3,6 +3,7 @@ package by.tms.lesson18.homework.listrealization;
 import by.tms.lesson18.homework.IntList;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class ArrayListIterator implements Iterator<Integer> {
 
@@ -21,7 +22,11 @@ public class ArrayListIterator implements Iterator<Integer> {
     }
 
     @Override
-    public Integer next() {
-        return list.get(++index - 1);
+    public Integer next() throws NoSuchElementException {
+        if(hasNext()) {
+            return list.get(++index - 1);
+        }else{
+            throw new NoSuchElementException();
+        }
     }
 }
--- src/by/tms/lesson18/homework/listrealization/LinkedListIterator.java
+++ src/by/tms/lesson18/homework/listrealization/LinkedListIterator.java
@@ -3,6 +3,7 @@ package by.tms.lesson18.homework.listrealization;
 import by.tms.lesson18.homework.entity.Node;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class LinkedListIterator implements Iterator<Integer> {
 
@@ -18,9 +19,13 @@ public class LinkedListIterator implements Iterator<Integer> {
     }
 
     @Override
-    public Integer next() {
-        Integer returnedElement = intLinkedNode.getElement();
-        intLinkedNode = intLinkedNode.getNextNode();
-        return returnedElement;
+    public Integer next() throws NoSuchElementException {
+        if(hasNext()) {
+            Integer returnedElement = intLinkedNode.getElement();
+            intLinkedNode = intLinkedNode.getNextNode();
+            return returnedElement;
+        }else{
+            throw new NoSuchElementException();
+        }
     }
 }
